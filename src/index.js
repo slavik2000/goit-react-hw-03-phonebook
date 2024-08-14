26852c405d455512af1d8b81a1e6c7b2f374998a
sяva <slavik.bieriezhnoi1995@gmail.com>
2024-08-14 20:20:09 +0300

    add new file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
+
 import React from 'react';
+import { ThemeProvider } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+import App from 'components/App';
 import './index.css';
+
+const theme = {
+  colors: {
+    backgroundColor: '#fff',
+    sectionBackgroundColor: '#ccc',
+    buttonBackgroundColor: '#999',
+  },
+  containerBorderRadius: '0px',
+  inputBorderRadius: '8px',
+  buttonBorderRadius: '5px',
+  buttonBorder: '1px solid #bb9665',
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
