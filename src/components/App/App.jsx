fc76125dd4618afedc3d8305495626620247e265
sяva <slavik.bieriezhnoi1995@gmail.com>
2024-08-14 21:43:19 +0300

    remove file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,6 +23,23 @@ class App extends Component {
     filter: '',
   };
 
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem('contacts'); 
+  //   const parsedContacts = JSON.parse(contacts); 
+
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts }); 
+  //   }
+  // }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      // Сравниваем текущие контакты с предыдущим обьектом контактов.
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      // Если контакты изменились, сохраняем их в localStorage.
+    }
+  }
+
   addContact = ({ name, number }) => {
     const contact = { id: nanoid(), name, number };
     const normalizedName = name.toLowerCase();
